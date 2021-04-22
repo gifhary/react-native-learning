c80454c019cba3570b6b405e45adb6a645e1c809
raymondchiu10 <raymondchiu10@gmail.com>
2021-04-21 17:01:35 -0700

    Update MessagesMain.tsx

--- mobile/src/screens/messages/MessagesMain.tsx
+++ mobile/src/screens/messages/MessagesMain.tsx
@@ -3,45 +3,49 @@ import React, {useLayoutEffect} from "react";
 import globalStyles from "../../theme/globalStyles";
 import SafeAreaView from "react-native-safe-area-view";
 import {StackNavigationProp} from "@react-navigation/stack";
-import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
 import MessageBubble, {EMessageProfilePosition} from "../../components/elements/MessageBubble";
-import {View} from "react-native";
+import {ScrollView, useWindowDimensions, View} from "react-native";
 import IndoButton from "../../components/buttons/IndoButton";
+import {IndoTextInput} from "../../components/inputs/IndoInput";
 
 interface IProps {
-	navigation: StackNavigationProp<any>;
+    navigation: StackNavigationProp<any>;
 }
 
-const placeholder = {
-	text: "Do you have a question? Select from our frequently asked questions below or create your own.",
-	source: "https://via.placeholder.com/300",
-	app: true,
+const initialMessageRender = {
+    text: "Do you have a question? Select from our frequently asked questions below or create your own.",
+    source: "https://via.placeholder.com/300",
+    app: true,
 }
 
 const MessagesMain: React.FC<IProps> = (props) => {
 
-	useLayoutEffect(() => {
-		props.navigation.setOptions({
-			headerTitle: ""
-		});
-	}, [props.navigation]);
+    const window = useWindowDimensions();
+    useLayoutEffect(() => {
+        props.navigation.setOptions({
+            headerTitle: ""
+        });
+    }, [props.navigation]);
 
-	return (
-		<SafeAreaView style={globalStyles.safeArea}>
-			<KeyboardAwareScrollView style={globalStyles.pagePadding}>
-			<IndoText style={globalStyles.h1}>Messages</IndoText>
-				<View>
-					<View>
-						<MessageBubble text={placeholder.text} source={{uri: placeholder.source}} imagePosition={placeholder.app ? EMessageProfilePosition.LEFT : EMessageProfilePosition.RIGHT} />
-					</View>
-					<View>
-						<IndoButton>Hello</IndoButton>
-					</View>
-				</View>
+    return (
+        <SafeAreaView style={globalStyles.safeArea}>
 
-			</KeyboardAwareScrollView>
-		</SafeAreaView>
-	);
+            <View style={globalStyles.pagePadding}>
+                <IndoText style={globalStyles.h1}>Messages</IndoText>
+                <ScrollView style={{height: "72%", borderWidth: 1}}>
+                    <MessageBubble text={initialMessageRender.text} source={{uri: initialMessageRender.source}}
+                                   imagePosition={initialMessageRender.app ? EMessageProfilePosition.LEFT : EMessageProfilePosition.RIGHT}/>
+                </ScrollView>
+            </View>
+                <View style={{alignItems: "flex-end"}}>
+                    <IndoButton>Hello</IndoButton>
+                    <IndoButton>Hello</IndoButton>
+                    <View style={{width: "61%", paddingHorizontal: 10}}>
+                        <IndoTextInput>Ask a Custom Question</IndoTextInput>
+                    </View>
+                </View>
+        </SafeAreaView>
+    );
 };
 
 export default MessagesMain;
